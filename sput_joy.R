1340117de65999cef00df6ac0b7e2456ef6a097b
mac7678 <mcasillas78@yahoo.com>
2018-01-06 14:55:44 -0800

    bonus plot added

--- sput_joy.R
+++ sput_joy.R
@@ -4,21 +4,21 @@ library(wesanderson)
 library(broom)
 
 # functions -----
-sput_wavg <- function(dat){
+sput_wavg <- function(dat, weights=c(1,3,2,1)){
   dat <- dat %>% 
     group_by(Band_Album) %>%
     summarise(w.mean = weighted.mean(c(weighted.mean(Rating,sqrt(ratings)),
                                        weighted.mean(Rating,sqrt(reviews)),
                                        weighted.mean(Rating,sqrt(lists)),
                                        weighted.mean(Rating,sqrt(comments))),
-                                     c(1,3,2,1))
+                                     weights)
     ) %>%
     ungroup()
   return(dat)
 }
-boot_albs <- function(dat){
+boot_albs <- function(dat, weights = c(1,3,2,1)){
   dat <- dat[sample(1:nrow(dat), replace = TRUE), ] %>%
-    sput_wavg()
+    sput_wavg(weights)
   return(dat)
 }
 # Script ----
@@ -105,3 +105,66 @@ p <- p +
         axis.title = element_blank())
 p +
   ggsave(paste0(joy_dir, 'faithful_joyplot.png'),height = 9, width = 4)
+
+## BONUS, Unknown Pleasure's JoyPlot ----
+joy_dat <- read_csv('Joy-Division-Unknown-Pleasures.csv') %>%
+  mutate(ratings = map_dbl(ratings, ~ifelse(.x %>% is.na(), 1L, .x)) %>% as.integer) %>%
+  mutate_if(is.numeric, ~ifelse(is.na(.x), 0, .x))
+
+set.seed(556666777)
+weights <- tibble(names = as.character(1:50),
+                  w1 = runif(50),
+                  w2 = runif(50),
+                  w3 = runif(50),
+                  w4 = runif(50))
+joy <- list()
+boots <- 1000
+for(i in 1:nrow(weights)){
+  tmp_weights <- weights[i, 2:5 ] %>% as_vector()
+  joy[[i]] <- map_df(1:boots, ~ joy_dat %>%
+                                 boot_albs(tmp_weights)) %>%
+    mutate(Band_Album = weights$names[i])
+  print(paste0(i, ' COMPLETED'))
+}
+joy <- joy %>%
+  bind_rows()
+# it's copypasted, blah i know
+j3 <- joy %>% 
+  mutate(Band_Album = Band_Album %>% as.integer(),
+         B_A = rev(as.numeric(Band_Album)),
+         w.mean = scale(w.mean)[,1]) %>% 
+  arrange(Band_Album) %>%
+  rename(x = w.mean) %>%
+  group_by(Band_Album, B_A) %>% 
+  do(tidy(density(.$x, n = 100))) %>% 
+  group_by() %>% 
+  mutate(ymin = B_A * (max(y) / 7), #This constant controls how much overlap between groups there is
+         ymax = y + ymin)
+
+
+j4 <- j3 %>% 
+  group_by(Band_Album, B_A) %>% 
+  do(data.frame(approx(.$x, .$ymax, xout = seq(min(j3$x), max(j3$x), length.out = 250)))) %>% 
+  mutate(y = ifelse(is.na(y), j3$ymin[j3$Band_Album == Band_Album][1], y),
+         ymin = j3$ymin[j3$Band_Album == Band_Album][1],
+         ymaxN = y + rnorm(n(), 0.001, 0.005)) %>% 
+  arrange(x) %>% 
+  mutate(ymaxN = ifelse(row_number() %in% c(1, n()), ymin + min(ymaxN - ymin), ymaxN))
+
+j4$ymaxS <- smooth(j4$ymaxN, kind = "S", endrule = "copy", do.ends = FALSE)
+p <- ggplot()
+for (i in rev(unique(j4$Band_Album))) {
+  p <- p + geom_ribbon(data = j4[j4$B_A == i,], 
+                       aes(x = x, ymin = ymin + min(j4$ymaxN - j4$ymin), ymax = ymaxS, group = B_A), 
+                       colour = "#F0F0F0", fill = "black") +
+    geom_hline(yintercept = j4$ymin[j4$B_A == i][1] + min(j4$ymaxN - j4$ymin), colour = "#000000")
+}
+p <- p + 
+  coord_fixed(4) +
+  theme(panel.grid = element_blank(),
+        panel.background = element_rect(fill = "#000000"),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title = element_blank())
+p +
+  ggsave(paste0(joy_dir, 'joyplot_joyplot.png'),height = 9, width = 4)
